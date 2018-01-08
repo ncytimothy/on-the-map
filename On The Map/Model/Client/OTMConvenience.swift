deb4fd1711a39c322e75ced40a95eec80fd31235
Timothy Ng <ncy.timothy@gmail.com>
2018-01-08 12:53:47 -0800

    Add Authentication Flow

---
+++ On The Map/Model/Client/OTMConvenience.swift
@@ -0,0 +1,29 @@
+//
+//  OTMConvenience.swift
+//  On The Map
+//
+//  Created by Timothy Ng on 1/8/18.
+//  Copyright © 2018 Timothy Ng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - OTMClient (Convenience Resource Methods)
+
+extension OTMClient {
+    
+    // MARK: Authentication Methods
+    /*
+     AUTHENTICATION FLOW:
+     
+     1. Post a new login session
+     2. Get the session ID
+ 
+ 
+ 
+ 
+     */
+    
+    
+}
